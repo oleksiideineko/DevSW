8173235e0eac01c99082303445099355f613024a
oleksiy <lexadein@gmail.com>
2024-01-30 22:27:38 +0200

    viva MechMat

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+def vivaMechMat():
+    return "Viva Mech-mat faculty!!!"
 def Hello():
     return "Hello, world!"
 
 
-for i in range (10):
-    print(Hello())
-    
+#for i in range (10):
+#    print(Hello())
+print(vivaMechMat())    
